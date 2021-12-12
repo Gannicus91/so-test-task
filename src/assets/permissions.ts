04fa1dc3a89956a1eeb0631c03924e37ae317cb4
m.dzhivanyan <m.dzhivanyan@brandquad.ru>
2021-12-12 11:09:30 +0400

    add permissions data

---
+++ src/assets/permissions.ts
@@ -0,0 +1,74 @@
+export default [
+  {
+    id: 1,
+    parentId: null,
+    name: 'node 1',
+    checked: true,
+  },
+  {
+    id: 2,
+    parentId: null,
+    name: 'node 2',
+    checked: true,
+  },
+  {
+    id: 3,
+    parentId: null,
+    name: 'node 3',
+    checked: true,
+  },
+  {
+    id: 4,
+    parentId: null,
+    name: 'node 4',
+    checked: true,
+  },
+  {
+    id: 5,
+    parentId: 1,
+    name: 'node 1.1',
+    checked: true,
+  },
+  {
+    id: 6,
+    parentId: 1,
+    name: 'node 1.2',
+    checked: true,
+  },
+  {
+    id: 7,
+    parentId: 1,
+    name: 'node 1.3',
+    checked: true,
+  },
+  {
+    id: 8,
+    parentId: 1,
+    name: 'node 1.4',
+    checked: true,
+  },
+  {
+    id: 9,
+    parentId: 5,
+    name: 'node 1.1.1',
+    checked: true,
+  },
+  {
+    id: 10,
+    parentId: 9,
+    name: 'node 1.1.1.1',
+    checked: true,
+  },
+  {
+    id: 11,
+    parentId: 10,
+    name: 'node 1.1.1.1.1',
+    checked: true,
+  },
+  {
+    id: 12,
+    parentId: 10,
+    name: 'node 1.1.1.1.2',
+    checked: true,
+  },
+];
